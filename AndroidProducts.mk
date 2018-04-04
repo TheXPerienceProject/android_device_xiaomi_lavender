c7a510be795a64535afd8d50b973d4809d59a1c6
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    lavender: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633
    Signed-off-by: klozz <klozz@vivaldi.net>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,3 +7,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_lavender.mk \
     $(LOCAL_DIR)/xperience_lavender.mk
+
+COMMON_LUNCH_CHOICES := \
+    xperience_lavender-user \
+    xperience_lavender-userdebug \
+    xperience_lavender-eng
