41cd747a3a28380adf56a7b334fe2af58f38ce7e
Ady Abraham <adyabr@google.com>
2019-07-24 11:05:44 -0700

    lavender: enable GL comp backpressure Enable backpressure for client composition to avoid jank due to HWC queue stuffing.
    Test: 1. Open Camera
          2. Swipe horizontally to go back to previous app.
    
    Bug: 138083790
    Change-Id: I3d26aa470c1660b23bd97133f39858f92638aca8
    Signed-off-by: klozz <klozz@vivaldi.net>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -151,6 +151,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.enable_hwc_vds=1 \
+    debug.sf.enable_gl_backpressure=1 \
     debug.sf.early_app_phase_offset_ns=1500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
