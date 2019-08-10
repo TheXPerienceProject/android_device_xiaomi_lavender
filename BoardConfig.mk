6384dbda24acb1346d6807aa8ade229e743a59a7
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2019-08-10 17:14:22 -0500

    lavender: kernel clang \m/

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lavender
 TARGET_KERNEL_CONFIG := lineage-lavender_defconfig
+TARGET_KERNEL_CLANG_COMPILE := true
 
 # dtbo
 BOARD_KERNEL_SEPARATED_DTBO := true
