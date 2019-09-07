00a6e7ae422957fc3e95abcc3966560b2cd09fd3
klozz <klozz@vivaldi.net>
2019-09-07 06:58:49 +0000

    lavender: update media profiles
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -286,7 +286,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     $(LOCAL_PATH)/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
     $(LOCAL_PATH)/media/media_profiles_sdm660_v1.xml:system/etc/media_profiles_sdm660_v1.xml \
-    $(LOCAL_PATH)/media/media_profiles_sdm660_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_sdm660_v1.xml
+    $(LOCAL_PATH)/media/media_profiles_sdm660_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_sdm660_v1.xml \
+    $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
 
 # Net
