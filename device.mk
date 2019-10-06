b8d8a6dc4184dcc9bab91b5ba8d9ec49777dcb8a
klozz <klozz@vivaldi.net>
2019-10-06 03:56:05 +0000

    lavender: not functional change

--- device.mk
+++ device.mk
@@ -223,6 +223,7 @@ PRODUCT_PACKAGES += \
     libmediadrm \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.widevine \
     android.hardware.drm@1.2-service.clearkey
 
 # Fingerprint feature
