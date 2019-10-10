ca9f371a564fd5952ea5fd220f20098bb716d5b2
klozz <klozz@vivaldi.net>
2019-10-10 10:22:56 +0000

    lavender: revert: build keymaster from OSS (use prebuilt)
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -302,12 +302,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# Keymaster
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service \
-    android.hardware.keymaster@4.0-service
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_lavender \
