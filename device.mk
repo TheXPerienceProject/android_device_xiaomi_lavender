d91e30aede4752dde8b2fc611ddabbe39014d237
klozz <klozz@vivaldi.net>
2019-10-15 08:10:44 +0000

    lavender: Build SnapdragonCamera
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -139,7 +139,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
     libcameraservice \
-    Camera2 \
+    SnapdragonCamera \
     vendor.qti.hardware.camera.device@1.0
 
 # Codec2 modules
