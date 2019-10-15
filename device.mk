cc8c5bb9f6e7090a60d713b1a91f4e7e7fc9bbff
klozz <klozz@vivaldi.net>
2019-10-15 10:36:00 +0000

    lavender: revert to camera2 until i fix fucking apex bug in SnapdragonCamera
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -140,7 +140,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     libxml2 \
     libcameraservice \
-    SnapdragonCamera \
+    Camera2 \
     vendor.qti.hardware.camera.device@1.0
 
 # Codec2 modules
