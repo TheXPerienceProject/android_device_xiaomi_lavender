26dc75b2a72aa31955828f0135f6e8cd10198ec3
klozz <klozz@vivaldi.net>
2019-12-03 08:35:41 +0000

    lavender: specify OUT_PRODUCT
    Signed-off-by: klozz <klozz@vivaldi.net>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -231,6 +231,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/dsp:/dsp \
     /vendor/firmware_mnt:/firmware \
     /vendor/bt_firmware:/bt_firmware
+TARGET_COPY_OUT_PRODUCT := system/product
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USES_MKE2FS := true
 
