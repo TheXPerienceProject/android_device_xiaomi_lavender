1c4e1b059fe5585654dbb87ffbf9cdcfb9500201
Himanshu Agrawal <hagraw@codeaurora.org>
2020-08-17 13:07:28 +0530

    sdm660-common: Disable ahardware buffer
    - Disable ahardware buffer usage in gralloc
    
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- properties.mk
+++ properties.mk
@@ -127,6 +127,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.cabl=0 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
+    vendor.gralloc.disable_ahardware_buffer=1 \
     vendor.gralloc.enable_fb_ubwc=1
 
 # FRP
