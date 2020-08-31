664b017b4f38b22eb194c682ead7fb8eee165a2e
klozz <klozz@vivaldi.net>
2020-08-31 09:53:39 +0000

    lavender: Upgrade to thermal 2.0
    [*] Lavender doesn't support trips but i no remove it due to future usage on other device's
    [*] Configured as per configs extracted from
    Cooling_devics: cat /sys/devices/virtual/thermal/cooling_device[0-9]/type
    tsens_tz_sensors extracted from kernel and thermal hal
    
    static char *cpu_sensors_660[] =
    {
        "tsens_tz_sensor1",
        "tsens_tz_sensor1",
        "tsens_tz_sensor1",
        "tsens_tz_sensor1",
        "tsens_tz_sensor3",
        "tsens_tz_sensor4",
        "tsens_tz_sensor5",
        "tsens_tz_sensor6",
    };
    
    static char *misc_sensors_660[] =
    {
        "tsens_tz_sensor8",
        "battery",
        "xo_therm"
    };
    
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -542,9 +542,13 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
+    android.hardware.thermal@2.0-service.lavender \
+    android.hardware.thermal@2.0 \
+    android.hardware.thermal@2.0.vendor \
     thermal.sdm660
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # Touchscreen
 PRODUCT_PACKAGES += \
