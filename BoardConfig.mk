b7344b583f8f7d5e2e7b66287fb578b000076bf8
klozz <klozz@vivaldi.net>
2020-09-15 09:19:33 +0000

    lavender: decouple vbmeta.img from recovery.img for non-A/B devices as required https://github.com/aosp-mirror/platform_build/commit/1aeef725a75af4d48b98b17175563ee442d45d1b
    Signed-off-by: klozz <klozz@vivaldi.net>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,10 @@ BOARD_AVB_ENABLE := true
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 2099-12-20
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 2
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1
