b10df80f79194153ce2bd8bc27f54195d5b245b1
klozz <klozz@vivaldi.net>
2020-09-19 05:19:41 +0000

    lavender: update drm to 1.3
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -262,8 +262,8 @@ PRODUCT_PACKAGES += \
     libmediadrm \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.widevine \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.widevine \
+    android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint feature
 PRODUCT_PACKAGES += \
