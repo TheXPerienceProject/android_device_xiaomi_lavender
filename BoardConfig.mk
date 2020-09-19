2b83fc821a79066260815d7040eac65cefbbf824
klozz <klozz@vivaldi.net>
2020-09-19 05:11:49 +0000

    lavender: Add qcom sepolicy
    Signed-off-by: klozz <klozz@vivaldi.net>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -269,6 +269,7 @@ BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # SELinux
 #include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 #BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
