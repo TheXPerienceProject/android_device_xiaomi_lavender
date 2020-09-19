0b98c9928268755afea38ec86177ffedca760d9d
Danny Lin <danny@kdrag0n.dev>
2020-09-18 22:47:02 -0700

    lavender: Switch Skia rendering backend to Vulkan
    This results in slightly lower frame times in most UiBench tests:
    
    Skia + Adreno OpenGL:
                                Score   Jank Frames Bad Frames  Avg Frame Time (ms)
    Overdraw Test               1       0.01%       0.06%       3.16
    Edit Text Input             5       0.13%       4.99%       7.79
    High-hitrate text render    1       0.01%       0.51%       6.81
    Image List View Fling       1       0.01%       0.32%       6.73
    Bitmap Upload Test          0       0.01%       0.99%       9.38
    Low-hitrate text render     1       0.01%       0.49%       7.08
    List View Fling             23      0.12%       0.78%       6.61
    Shadow Grid Fling           16      0.10%       0.94%       7.32
    
    Skia + Adreno Vulkan:
                                Score   Jank Frames Bad Frames  Avg Frame Time (ms)
    Overdraw Test               1       0.01%       0.06%       2.94
    Edit Text Input             6       0.13%       4.60%       8.00
    High-hitrate text render    1       0.01%       0.28%       6.29
    Image List View Fling       1       0.01%       0.36%       5.96
    Bitmap Upload Test          1       0.01%       0.15%       9.25
    Low-hitrate text render     1       0.01%       0.33%       6.71
    List View Fling             16      0.09%       2.70%       6.62
    Shadow Grid Fling           11      0.07%       0.49%       6.98
    
    Change-Id: Idb23fca4d801746588a7ba63231705a7f9621b35
    
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -207,6 +207,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.hwui.renderer=skiavk \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.early_app_phase_offset_ns=500000 \
