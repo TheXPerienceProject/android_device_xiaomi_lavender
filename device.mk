fe3b4cc084fd3aaf3ecbc67afb4f34cef951d38c
klozz <klozz@vivaldi.net>
2020-09-26 01:10:48 +0000

    lavender: move to radio 1.5, config 1.2, secure_element 1.2 add deprecated to manifest too
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -508,9 +508,9 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.2 \
-    android.hardware.radio.config@1.0 \
-    android.hardware.secure_element@1.0 \
+    android.hardware.radio@1.5 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.secure_element@1.2 \
     librmnetctl \
     libxml2
 
