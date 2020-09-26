92ebb976ae6bd73d87897061127a5d875501e838
klozz <klozz@vivaldi.net>
2020-09-26 01:30:58 +0000

    lavender: add ant targets
    Signed-off-by: klozz <klozz@vivaldi.net>

--- device.mk
+++ device.mk
@@ -156,11 +156,11 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_dun@1.0
 
 # ANT+
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
     AntHalService \
     com.dsi.ant.antradio_library
 
-#PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Camera
