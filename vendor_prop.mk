a3b043188cc037a3c1047119d5024c24847da93e
klozz <klozz@vivaldi.net>
2020-09-26 03:15:07 +0000

    lavender: use dex2oat64 reference: https://android.googlesource.com/platform/frameworks/native/+/master/cmds/installd/dexopt.cpp#1766
    Signed-off-by: klozz <klozz@vivaldi.net>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -20,7 +20,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
     dalvik.vm.image-dex2oat-cpu-set=4,5,6,7 \
-    dalvik.vm.dex2oat-threads=4
+    dalvik.vm.dex2oat-threads=4 \
+    dalvik.vm.dex2oat64.enabled=true
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
