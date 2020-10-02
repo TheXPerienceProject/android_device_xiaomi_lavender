524568059e1616a9ea4049b7078b0c3400a9dc8a
OdSazib <odsazib@gmail.com>
2020-10-02 10:35:45 +0530

    lavender: remove unnecessary deps from vndk
    Co-authored-by: Nebrassy <nebras30@gmail.com>
    Co-authored-by: Anush02198 <Anush.4376@gmail.com>
    Change-Id: I25982798d27a207d6b5d0343989888ecee22d457
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- vndk/vndk-ext-libs.mk
+++ vndk/vndk-ext-libs.mk
@@ -3,8 +3,6 @@ EXTRA_VENDOR_LIBRARIES_32 := \
     libaudioclient \
     libaudiomanager \
     libaudiopolicy \
-    libbinder \
-    libbinderthreadstate \
     libbufferhub \
     libbufferhubqueue \
     libcamera_client \
@@ -12,7 +10,6 @@ EXTRA_VENDOR_LIBRARIES_32 := \
     libgui \
     libharfbuzz_ng \
     libheif \
-    libicuuc \
     libicui18n \
     libinput \
     libmedia \
@@ -29,6 +26,5 @@ EXTRA_VENDOR_LIBRARIES_32 := \
     libvibrator
 
 EXTRA_VENDOR_LIBRARIES_64 := \
-    libicuuc \
     libstdc++ \
     libpowermanager
