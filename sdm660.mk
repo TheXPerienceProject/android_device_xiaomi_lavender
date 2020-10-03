63c29950923591c3de908a3ba4a473226cf974b6
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-10-03 13:20:23 +0530

    sdm660-common: Update dexpreopt configuration
     * strip off as much as the debugging as we can
    
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -455,6 +455,16 @@ endif
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
 
+# Dex
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_DEBUG := false
+endif
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
