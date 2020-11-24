b668d3812220824687f936a42025d4cfed5bb121
klozz <klozz@TheXPerienceProject.org>
2020-11-24 13:14:56 +0100

    lavender: build QTI wifi suplicant 2.2 and hostapd1.2
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- device.mk
+++ device.mk
@@ -643,10 +643,9 @@ PRODUCT_PACKAGES += \
 
 # Wi-FI caf
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.wifi.hostapd@1.1 \
-    vendor.qti.hardware.wifi.hostapd@1.0 \
+    vendor.qti.hardware.wifi.hostapd@1.2 \
     vendor.qti.hardware.wifi@1.0 \
-    vendor.qti.hardware.wifi.supplicant@2.1
+    vendor.qti.hardware.wifi.supplicant@2.2
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
