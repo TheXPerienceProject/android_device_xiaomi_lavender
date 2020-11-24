88f65f0ff99341251db84eb9df4ea1a7a62164e0
klozz <klozz@TheXPerienceProject.org>
2020-11-24 11:13:33 +0100

    lavender: build btconfigstore from source
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- device.mk
+++ device.mk
@@ -156,6 +156,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0 \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0 \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0 \
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
     vendor.qti.hardware.bluetooth_dun@1.0
 
 # Component overrides
