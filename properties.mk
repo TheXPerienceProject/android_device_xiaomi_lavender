a24b8d6e892b866328197e97ecbb9cf32e52be49
alanndz <alanmahmud0@gmail.com>
2020-12-08 19:57:58 +0000

    sdm660-common: move lmkd property to vendor init
    Logs:
    12-09 19:24:26.703 I/ActivityManager(1489): Killing 13986:com.opera.browser:privileged_process0/u0a218 (adj 700): remove task
    12-09 19:25:13.665 I/ActivityManager(1489): Killing 10073:com.google.android.apps.maps:server_recovery_process/u0a202 (adj 985): empty #17
    
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- properties.mk
+++ properties.mk
@@ -139,20 +139,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.vt_avail_ovr=1  \
     persist.dbg.wfc_avail_ovr=1
 
-# LMKD
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.lmk.low=1001 \
-    ro.lmk.medium=800 \
-    ro.lmk.critical=0 \
-    ro.lmk.critical_upgrade=false \
-    ro.lmk.upgrade_pressure=100 \
-    ro.lmk.downgrade_pressure=100 \
-    ro.lmk.kill_heaviest_task=true \
-    ro.lmk.kill_timeout_ms=100 \
-    ro.lmk.use_minfree_levels=true \
-    ro.lmk.log_stats=true \
-    ro.lmk.use_psi=true
-
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
