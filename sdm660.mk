d4095ee55efd86e5a55b2f0231597239b9ffe987
Carter Hsu <carterhsu@google.com>
2022-02-08 17:33:35 +0800

    sdm660-common: Update Audio HIDL to 7.1
    Bug: 205235270
    Test: build pass
    Signed-off-by: Carter Hsu <carterhsu@google.com>
    Change-Id: Ic54e2787ef27022556430c6c1db346b1e1348e39
    Signed-off-by: pix106 <sbordenave@gmail.com>
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -70,7 +70,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl:32 \
+    android.hardware.audio@7.1-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.1-impl \
