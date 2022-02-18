1c7674831a83203ee5bd24806af23b4a732cfd10
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-02-19 00:15:32 +0530

    sdm660-common: don't build vndk.current on vendor
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -588,8 +588,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libdng_sdk.vendor_32 \
     libstdc++.vendor_32  \
-    vndk-ext \
-    com.android.vndk.current.on_vendor
+    vndk-ext
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
