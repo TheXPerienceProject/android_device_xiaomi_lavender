b83c50154eddc9f890fc6c8399963a18d4cf744f
wHoEMi <ehteshammalik18998@gmail.com>
2022-07-06 10:39:04 +0530

    sdm660-common: debloat some google apps
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- Recorder/Android.mk
+++ Recorder/Android.mk
@@ -7,5 +7,5 @@ LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_MODULE_PATH := $(TARGET_OUT)/priv-app
-LOCAL_OVERRIDES_PACKAGES := Recorder
+LOCAL_OVERRIDES_PACKAGES := Recorder RecorderPrebuilt TurboPrebuilt CalendarGoogle Gmail2 Maps YouTube DevicePersonalizationPrebuiltPixel2021 TurboAdapter
 include $(BUILD_PREBUILT)
