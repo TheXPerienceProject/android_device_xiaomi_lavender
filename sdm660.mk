661deebd1801921eb05c77c27b5b3f54a2b006ce
electimon <electimon@gmail.com>
2022-08-16 09:43:13 +0800

    sdm660-common: Remove audio.a2dp.default from PRODUCT_PACKAGES
    * This module is not to be built this way anymore
    
    Signed-off-by: electimon <electimon@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -74,7 +74,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.1-impl \
-    audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
