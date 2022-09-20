e8929875f039eaa69c69c6182961c0d6d07c0126
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-09-21 03:16:57 +0530

    Revert "sdm660-common: Switch back to basic USB HAL"
    This reverts commit 20d4d0af1cac5bfdc996696a0040ebf047ab5522.

--- sdm660.mk
+++ sdm660.mk
@@ -592,7 +592,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.xiaomi_sdm660
 
 # VNDK
 PRODUCT_PACKAGES += \
