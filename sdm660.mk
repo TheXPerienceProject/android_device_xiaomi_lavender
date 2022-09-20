039ef6921b3f940ecc5db0ccf725b35e1666e471
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-09-21 05:18:07 +0530

    sdm660-common: Switch to CAF/CLO Powerhal
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -441,17 +441,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi_sdm660-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock
-
-# Powerhint
-ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
-    PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/power-libperfmgr/sdm636_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-else
-    PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/power-libperfmgr/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-endif
+    android.hardware.power-service \
+    android.hardware.power-impl \
+    power.qcom
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
@@ -551,9 +543,7 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(COMMON_PATH) \
-    hardware/google/interfaces \
-    hardware/google/pixel
+    $(COMMON_PATH)
 
 # Sqlite
 PRODUCT_PACKAGES += \
