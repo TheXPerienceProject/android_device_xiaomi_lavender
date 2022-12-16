5996af13349f138020f21d6db8845a02bada7b4d
Luofan Chen <clfbbn@gmail.com>
2022-12-16 22:56:41 +0530

    sdm660-common: ship android.system.keystore2
    Fixes: Could not find 'aidl/android.system.keystore2.IKeystoreService/default'
           for ctl.interface_start from pid: 617 (/system/bin/servicemanager)

--- sdm660.mk
+++ sdm660.mk
@@ -338,6 +338,10 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(COMMON_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Keystore
+PRODUCT_PACKAGES += \
+    android.system.keystore2
+    
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm660
