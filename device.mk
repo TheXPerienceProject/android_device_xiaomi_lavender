21fd7f3bc92528f4295b33f998263791a96b4b12
Shreedhan003 <shridhan98@gmail.com>
2022-12-26 12:16:03 +0530

    lavender: Drop unused DRM packages
    - Also drop from HIDL

--- device.mk
+++ device.mk
@@ -195,14 +195,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.1.vendor \
-    android.hardware.drm@1.0.vendor
+    android.hardware.drm@1.4.vendor
 
 # FM
 PRODUCT_PACKAGES += \
