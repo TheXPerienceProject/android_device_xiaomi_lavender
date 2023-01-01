9bec743b34ea8b0f174dcd1acba98f9f5bf3ff4e
Shreedhan003 <shridhan98@gmail.com>
2023-01-01 17:55:51 +0530

    lavender: camera: Build some open source packages

--- device.mk
+++ device.mk
@@ -102,7 +102,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.common@1.0 \
     android.hardware.camera.device@3.5:64 \
+    android.hardware.camera.metadata@3.4:64 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
