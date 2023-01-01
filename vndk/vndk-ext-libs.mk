3937c61a3f2e503519a55ec3039b5b4681543074
jimmy <quesoo@gmail.com>
2023-01-01 11:54:06 +0530

    lavender: vndk: add vendor packages needed for camera hal
    Change-Id: I9caef5b5421cec04496b07d33c0f6fbabf0222db

--- vndk/vndk-ext-libs.mk
+++ vndk/vndk-ext-libs.mk
@@ -1,2 +1,34 @@
-EXTRA_VENDOR_LIBRARIES := \
+EXTRA_VENDOR_LIBRARIES_32 := \
+    libandroidicu \
+    libaudioclient \
+    libaudiomanager \
+    libaudiopolicy \
+    libbinder \
+    libbinderthreadstate \
+    libbufferhub \
+    libbufferhubqueue \
+    libcamera_client \
+    libft2 \
+    libgui \
+    libharfbuzz_ng \
+    libheif \
+    libicuuc \
+    libicui18n \
+    libinput \
+    libmedia \
+    libmediametrics \
+    libmediautils \
+    libmemunreachable \
+    libminikin \
+    libnblog \
+    libpackagelistparser \
+    libpdx_default_transport \
+    libselinux \
+    libstdc++ \
+    libsonivox \
+    libvibrator
+
+EXTRA_VENDOR_LIBRARIES_64 := \
+    libicuuc \
+    libstdc++ \
     libpowermanager
