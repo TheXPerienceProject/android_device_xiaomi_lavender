239e7586f3e0a43ece0c29eb0cede9fb9f4878bf
klozz <carlosj@klozz.dev>
2023-04-03 07:10:51 +0000

    lavender: add DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,6 +146,7 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE +=  $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
