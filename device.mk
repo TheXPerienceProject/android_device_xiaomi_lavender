3a991e398a8df31a997a85c1469c8a0cec68375a
klozz <carlosj@klozz.dev>
2023-04-03 19:34:56 -0600

    Revert "sdm660-common: Aosp-ify bluetooth"
    This reverts commit bef743808276bb59e4bbdc51e2673c0d4716c745.
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -33,6 +33,7 @@ PRODUCT_PACKAGES += \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
+    liba2dpoffload \
     libaudio-resampler \
     libhdmiedid \
     libhfp \
@@ -84,12 +85,14 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth@1.1.vendor \
     bt_stack.conf \
+    liba2dpoffload \
     libbthost_if \
     libhdmiedid \
     libhfp \
     libldacBT_bco \
     libldacBT_dec \
-    libsndmonitor
+    libsndmonitor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
@@ -116,10 +119,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
     libsfplugin_ccodec
-
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
 # Configstore
 PRODUCT_PACKAGES += \
