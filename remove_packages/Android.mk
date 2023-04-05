39240c54062e2a8765b73ccd28dd0dd791b86e18
klozz <carlosj@klozz.dev>
2023-04-04 22:20:10 -0600

    lavender: remove more packages
    Signed-off-by: klozz <carlosj@klozz.dev>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps YouTube Drive PrebuiltGmail YouTubeMusicPrebuilt PixelLiveWallpaperPrebuilt CalendarGoogle PixelWallpapers2021 RecorderPrebuilt XPerienceWallpapers Gmail2 Gmail FilesPrebuilt RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES := Maps DeskClockGoogle YouTube Drive PrebuiltGmail YouTubeMusicPrebuilt talkback DocumentsUIGoogle PixelLiveWallpaperPrebuilt CalendarGoogle PixelWallpapers2021 RecorderPrebuilt XPerienceWallpapers Gmail2 Gmail FilesPrebuilt RecorderPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
