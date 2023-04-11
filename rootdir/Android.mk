51327ed660bfea2ae41706a2b5b43457aa5f2efc
klozz <carlosj@klozz.dev>
2023-04-11 16:00:27 -0600

    lavender:  rootdir: build fstab to ramdisk as well
    Needed by dynamic partitioning [1].
    
    [1] https://source.android.com/devices/tech/ota/dynamic_partitions/implement#fstab-changes
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -106,12 +106,22 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 ifneq ($(filter lavender,$(TARGET_DEVICE)),)
 LOCAL_SRC_FILES    := etc/fstab_A_retro.qcom
+LOCAL_REQUIRED_MODULES := fstab.qcom_ramdisk
 else ifeq ($(ENABLE_AB), true)
 LOCAL_SRC_FILES    := etc/fstab_AB.qcom
 else
 LOCAL_SRC_FILES    := etc/fstab.qcom
 endif
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fstab.qcom_ramdisk
+LOCAL_MODULE_STEM := fstab.qcom
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
