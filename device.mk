11a318e439248a21a06ccc20f98aa22f2ce83863
klozz <carlosj@klozz.dev>
2023-04-14 14:00:00 -0600

    Revert "Revert "lavender: build qti thermal""
    This reverts commit d6f75e0967a81c0cfbe7592b047dec21067aaef3.

--- device.mk
+++ device.mk
@@ -597,7 +597,8 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.xiaomi_sdm660.qti
+    android.hardware.thermal@2.0 \
+    android.hardware.thermal@2.0-service.qti
 
 # Thermal Conf
 PRODUCT_COPY_FILES += \
