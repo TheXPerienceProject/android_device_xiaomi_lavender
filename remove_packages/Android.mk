d8ac06d185fed1d6a8258ec31e0d3fd62fab53ab
klozz <carlosj@klozz.dev>
2023-07-05 02:47:29 -0600

    lavender: remove more packages due to our gapps increased the size
    Signed-off-by: klozz <carlosj@klozz.dev>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive YouTubeMusicPrebuilt talkback CalendarGoogle RecorderPrebuilt XPerienceWallpapers RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES := CalendarGoogle Maps Drive YouTubeMusicPrebuilt PixelLiveWallpaperPrebuilt talkback CalendarGoogle RecorderPrebuilt XPerienceWallpapers RecorderPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
